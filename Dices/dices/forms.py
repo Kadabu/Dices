ffcc64dd413ab728af87cf262b79401c2d5029aa
Kadabu <kj.skurska@gmail.com>
2020-05-11 18:12:45 +0200

    Base.html

---
+++ Dices/dices/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.contrib.auth.models import User
+# from .models import
+
+class Game1Form(forms.Form):
+    pass
--- Dices/dices/views.py
+++ Dices/dices/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+#from .models import
+from .forms import Game1Form
+import random
 
 # Create your views here.
+
+class Game1(View):
+
+    def get(self, request):
+        form = Game1Form()
+        return render(request, "game_1_start.html")
+
+    def post(self, request):
+        numbers = [random.randrange(1,6) for x in range(6)]
+        result = 0
+        return render(request, "game_1_result.html", {"result": result, "numbers": numbers})
