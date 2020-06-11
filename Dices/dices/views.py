46e3bbbc0ca7eecc7d233933738111367dfa72f7
Kadabu <kj.skurska@gmail.com>
2020-06-11 16:09:58 +0200

    game_1_start.html ictures added

--- Dices/dices/views.py
+++ Dices/dices/views.py
@@ -25,7 +25,7 @@ class Start(View):
         numbers = []
         if scores.dices_amount == 5:
             numbers = [random.randrange(1,7) for x in range(5)]
-            Roll.objects.create(dice_1=numbers[0], dice_2=numbers[1], dice_3=numbers[2], dice_4=numbers[3], dice_5=numbers[4], game_no=1)
+            roll = Roll.objects.create(dice_1=numbers[0], dice_2=numbers[1], dice_3=numbers[2], dice_4=numbers[3], dice_5=numbers[4], game_no=1)
             """Define a loss"""
             if numbers.count(2)<3 and numbers.count(3)<3 and numbers.count(4)<3 and numbers.count(6)<3 and 1 not in numbers and 5 not in numbers:
                 return render(request, "loss.html", {"numbers": numbers})
@@ -58,7 +58,18 @@ class Game1(View):
         form = Game1ChoiceForm()
         roll = Roll.objects.get(game_no=1)
         scores = Scores.objects.get(player_no=1)
-        return render(request, "game_1_start.html", {"form": form, "roll": roll, "scores": scores})
+        roll_dices = []
+        if scores.dices_amount == 5:
+            roll_dices = [roll.dice_1, roll.dice_2, roll.dice_3, roll.dice_4, roll.dice_5]
+        if scores.dices_amount == 4:
+            roll_dices = [roll.dice_1, roll.dice_2, roll.dice_3, roll.dice_4]
+        if scores.dices_amount == 3:
+            roll_dices = [roll.dice_1, roll.dice_2, roll.dice_3]
+        if scores.dices_amount == 2:
+            roll_dices = [roll.dice_1, roll.dice_2]
+        if scores.dices_amount == 1:
+            roll_dices = [roll.dice_1]
+        return render(request, "game_1_start.html", {"form": form, "roll": roll, "scores": scores, "roll_dices": roll_dices})
 
     def post(self, request):
         """Obliczanie wyniku na podstawie wyboru z formularza i przekierowanie do strony, gdzie gracz wybiera, czy rzuca dalej"""
@@ -196,4 +207,5 @@ class Total(View):
 
     def get(self, request):
         scores = Scores.objects.get(player_no=1)
-        return render(request, "total.html", {"scores": scores})
+        roll = Roll.objects.get(game_no=1)
+        return render(request, "total.html", {"scores": scores, "roll": roll})
