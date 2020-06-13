c225a78232d10c0826aa40c76cd3b48862d24717
Kadabu <kj.skurska@gmail.com>
2020-06-13 13:52:38 +0200

    app.js

---
+++ Dices/dices/static/js/app.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.dice').forEach(item => {
+  item.addEventListener('click', event => {
+    item.style.display = "none";
+  })
+})
+
+document.querySelectorAll('.dice-1').forEach(item => {
+  item.addEventListener('click', event => {
+    item.style.display = "none";
+  })
+})
